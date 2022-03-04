58a76e54e73dd29676b9fad8dd031effacce967a
Myke Amorim <mykeleony147@gmail.com>
2022-03-04 00:13:25 -0300

    Create generics.java

---
+++ poo/polimorfismo/generics.java
@@ -0,0 +1,15 @@
+package poo.polimorfismo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import poo.veiculos.Veiculo;
+
+public class generics {
+    // Programação genérica, que funciona com diferentes tipos:
+    List<String> strings = new ArrayList<>();
+    List<Integer> inteiros = new ArrayList<>();
+    List<Veiculo> veiculos = new ArrayList<>();
+
+    // Outras listas genéricas...
+}
