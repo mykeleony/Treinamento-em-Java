7cc4eca160027da1d248f8cdc6418fa3d9d980bc
Myke Amorim <mykeleony147@gmail.com>
2022-03-06 22:46:48 -0300

    Create Grasnar.java

---
+++ poo/design_patterns/strategy/comportamentos_de_pato/Grasnar.java
@@ -0,0 +1,9 @@
+package design_patterns.strategy.comportamentos_de_pato.quackar;
+
+public class Grasnar implements CapacidadeDeGrasnar {
+    
+    @Override
+    public void quack() {
+       System.out.println("Quack!");
+    }
+}
