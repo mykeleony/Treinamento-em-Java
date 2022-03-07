0225af6165ba594569c1e49f32b6255ead8c2bb6
Myke Amorim <mykeleony147@gmail.com>
2022-03-06 22:47:06 -0300

    Create GrasnoMudo.java

---
+++ poo/design_patterns/strategy/comportamentos_de_pato/GrasnoMudo.java
@@ -0,0 +1,7 @@
+package design_patterns.strategy.comportamentos_de_pato.quackar;
+
+public class GrasnoMudo implements CapacidadeDeGrasnar {
+    public void quack() {
+        System.out.println("Sou um pato mudo, bro.");
+    }
+}
