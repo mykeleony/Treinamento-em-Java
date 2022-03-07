226790c7e47addaf55f7b34cd73bb4051e3de301
Myke Amorim <mykeleony147@gmail.com>
2022-03-06 22:45:36 -0300

    Create Pato.java

---
+++ poo/design_patterns/strategy/Pato.java
@@ -0,0 +1,59 @@
+package design_patterns.strategy;
+
+import design_patterns.strategy.comportamentos_de_pato.quackar.CapacidadeDeGrasnar;
+import design_patterns.strategy.comportamentos_de_pato.voar.CapacidadeDeVoar;
+
+public abstract class Pato {
+    String cor;
+  
+    // Estratégias para os comportamentos altamente variáveis "Capacidade de Voar" e "Capacidade de Grasnar":
+    CapacidadeDeGrasnar capacidadeDeGrasnar;
+    CapacidadeDeVoar capacidadeDeVoar;
+
+    public Pato(String cor) {
+        this.cor = cor;
+    }
+
+    abstract void display();    // Cada pato tem um visual diferente.
+
+    public void nadar() {
+        System.out.println("O pato está nadando!");
+    }
+
+    public void grasnar() {
+        capacidadeDeGrasnar.quack();
+    }
+
+    public void voar() {
+        capacidadeDeVoar.voar();
+    }
+
+    /* Métodos modificadores: */ 
+    public void setCapacidadeDeGrasnar(CapacidadeDeGrasnar cdg) {
+        capacidadeDeGrasnar = cdg;
+    }
+
+    public void setCapacidadeDeVoar(CapacidadeDeVoar cdv) {
+        capacidadeDeVoar = cdv;
+    }
+
+    public static void main(String[] args) {
+        Pato patoborrachudo = new PatoDeBorracha("Amarelo");
+
+        patoborrachudo.display();
+        patoborrachudo.grasnar();
+        patoborrachudo.nadar();
+
+        Pato marrecobranco = new PatoMarreco("Branco");
+
+        marrecobranco.display();
+        marrecobranco.grasnar();
+        marrecobranco.nadar();
+
+        Pato patoRobo = new PatoRobotico();
+        
+        patoRobo.display();
+        patoRobo.grasnar();
+        patoRobo.voar();
+    }
+}
